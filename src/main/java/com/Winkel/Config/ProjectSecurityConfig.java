e0326fa16515774afee0f1859fc77d72d0181f73
PANKAJ KUMAR MUNDA <50673726+pankajhike@users.noreply.github.com>
2024-08-01 16:01:17 +0530

    SecurityFilterChain

--- src/main/java/com/Winkel/Config/ProjectSecurityConfig.java
+++ src/main/java/com/Winkel/Config/ProjectSecurityConfig.java
@@ -4,10 +4,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails normalUser= User.withUsername("Vikash")
+                .password("viki")
+                .roles("ROLE_NORMAL")
+                .build();
+
+        UserDetails adminUser= User.withUsername("pankaj")
+                .password("pankaj")
+                .roles("ROLE_ADMIN")
+                .build();
+
+
+            return new InMemoryUserDetailsManager(normalUser,adminUser);
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
