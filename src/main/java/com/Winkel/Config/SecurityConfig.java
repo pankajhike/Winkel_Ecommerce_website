02198decf37c3d31cf15c3970ecc53bb1f121618
PANKAJ KUMAR MUNDA <50673726+pankajhike@users.noreply.github.com>
2024-08-07 01:45:05 +0530

    Implemented Resitration Controller

---
+++ src/main/java/com/Winkel/Config/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.Winkel.Config;
+
+import com.Winkel.Service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private  UserService userService;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return userService;
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider= new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf->csrf.disable())
+                .formLogin(login->{
+                    login.loginPage("/login").permitAll();
+                    login.defaultSuccessUrl("/index");
+                })
+                .authorizeHttpRequests(
+                        auth-> {
+                            auth.requestMatchers("/signup" ,"/css/**","/fonts/**","/img/**","/js/**","/sass/**","/Sources/**").permitAll();
+                            auth.anyRequest().authenticated();
+                        })
+                .build();
+    }
+}
---
+++ src/main/java/com/Winkel/Service/UserService.java
@@ -0,0 +1,35 @@
+package com.Winkel.Service;
+
+import com.Winkel.Model.User;
+import com.Winkel.Repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = repository.findByUsername(username);
+        if (user.isPresent()) {
+            User userObj = user.get();
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(userObj.getUsername())
+                    .password(userObj.getPassword())
+                    .roles("USER") // You can specify roles or authorities as needed
+                    .build();
+        } else {
+            throw new UsernameNotFoundException(username);
+        }
+    }
+}
